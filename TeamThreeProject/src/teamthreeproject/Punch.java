b34d6fd880c9fc4f81bd5962b8e64fde991a6860
Travis Cotney <tcotney96@gmail.com>
2017-10-10 20:29:19 -0500

    Worked on Punch

--- TeamThreeProject/src/teamthreeproject/Punch.java
+++ TeamThreeProject/src/teamthreeproject/Punch.java
@@ -10,11 +10,30 @@ import java.util.*;
  * @author Travis
  */
 public class Punch {
-    private int scheduleTime;
-    private int clockTime;
-    private int adjustedTime;
+    private Date scheduleTime;
+    private Date clockTime;
+    private Date adjustedTime;
     
-    public void SetScheduleTime(int n){
+    public void setScheduleTime(Date n){
         this.scheduleTime = n;
+    }
+    public void setClockTime(Date n){
+        this.clockTime = n;
+        //this.adjustedTime = clockTime.getShift();
+    }
+    public void setAdjustedTime(Date n){
+        this.adjustedTime = n;
+    }
+    public Date getSchdelueTime(){
+        return scheduleTime;
+        
+    }
+    public Date getClockTime(){
+        return clockTime;
+        
+    }
+    public Date getAdjustedTime(){
+        return adjustedTime;
+       
     }
 }
