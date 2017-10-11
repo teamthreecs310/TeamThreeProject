26a2e1d24e393209256a9d51f090b751bd0e11d0
Christopher Owen <cowen4@stu.jsu.edu>
2017-10-10 21:12:24 -0500

    Shift class and getShift() method working. Optimized TASDatabase class as well.

--- TeamThreeProject/src/teamthreeproject/TeamThreeProject.java
+++ TeamThreeProject/src/teamthreeproject/TeamThreeProject.java
@@ -13,14 +13,14 @@ public class TeamThreeProject {
         // TODO code application logic here
         TASDatabase db = new TASDatabase();
         
-        
+        /* for Shift testing
         Shift s1 = db.getShift(1);
         Shift s2 = db.getShift(2);
         Shift s3 = db.getShift(3);
         
         System.out.println(s1.toString());
         System.out.println(s2.toString());
-        System.out.println(s3.toString());
+        System.out.println(s3.toString()); */
         
         db.closeConnection();
     
