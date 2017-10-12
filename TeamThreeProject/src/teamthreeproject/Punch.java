dbc52167182f8903ff65f533b604e3c2effea080
tcotneyjsu <tcotney96@gmail.com>
2017-10-12 18:55:54 -0500

    Finished Punch for now.

--- TeamThreeProject/src/teamthreeproject/Punch.java
+++ TeamThreeProject/src/teamthreeproject/Punch.java
@@ -8,34 +8,13 @@ import java.util.*;
 public class Punch {
 
     private String adjusted_time_stamp;
-    private String id;
+    private int id;
     private String terminal_id;
     private String event_type_id;
     private String badge_id;
     private String original_time_stamp;
     private String event_data;
     private Calendar calendar;
-    
-    
-    public Punch(String id, String terminal_id, String badge_id,
-                 String original_time_stamp, String event_type_id, 
-                 String event_data, String adjusted_time_stamp) {
-        
-        calendar = toGregorian(original_time_stamp);
-        this.id = id;
-        this.terminal_id = terminal_id;
-        this.badge_id = badge_id;
-        
-        this.adjusted_time_stamp = adjusted_time_stamp;
-        this.badge_id = badge_id;
-        this.event_type_id = event_type_id;
-        this.id = id;
-        this.original_time_stamp = original_time_stamp;
-        this.terminal_id = terminal_id;
-        this.event_data = event_data;
-        
-        
-    }
     
     private Calendar toGregorian(String orignal_time_stamp) {
         
@@ -50,8 +29,47 @@ public class Punch {
         calendar.set(year, month, day, hour, minute, second);
         return calendar;
     }
+    public void setID(int n){
+        id= n;
+    }
+    public void setTerminalID(String s){
+        terminal_id = s;
+    }
     
-    private String getEventType(String event_type_id) {
+    public void setBadgeID(String s){
+        badge_id = s;
+    }
+    public void setOriginalTimeStamp(String s){
+        original_time_stamp = s;
+    }
+    public void setEventTypeID(String s){
+        event_type_id = s;
+    }
+    public void setEventData(String s){
+        event_data = s;
+    }
+    public void setAdjustedTimeStamp(String s){
+        adjusted_time_stamp = s;
+    }
+    public int getID(){
+        return id;
+    }
+    public String getTerminalID(){
+        return terminal_id;
+    }
+    public String getOriginalTimeStamp(){
+        return original_time_stamp;
+    }
+    public String getEventTypeID(){
+        return event_type_id;
+    }
+    public String getEventData(){
+        return event_data;
+    }
+    public String getAdjustedTimeStamp(){
+        return adjusted_time_stamp;
+    }
+    public String getEventType(String event_type_id) {
         if (event_type_id.equals("0")) 
             return "Clocked Out: ";
         else if (event_type_id.equals("1")) 
@@ -60,18 +78,7 @@ public class Punch {
             return "Timed Out: ";
     }
     
-    public String printOrignalTimeStamp() {
-        return "";
-    }
     
-    public void setAdjustedTime(String adj_time){
-        adjusted_time_stamp = adj_time;
-    }
-   
-    public String printAdjustedTime(){
-        return adjusted_time_stamp;
-       
-    }
     
     
     
