356d02ab4ee564cdb29dc1985e09261fceec416a
Christopher Owen <cowen4@stu.jsu.edu>
2017-10-15 11:57:27 -0500

    Shift class has been cleaned up and restructured.

--- TeamThreeProject/src/teamthreeproject/TeamThreeProject.java
+++ TeamThreeProject/src/teamthreeproject/TeamThreeProject.java
@@ -3,24 +3,15 @@ package teamthreeproject;
 
 /**
  *
- * @author Travis
+ * @author TeamThree
  */
 public class TeamThreeProject {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         TASDatabase db = new TASDatabase();
-        
-        /* for Shift testing
-        Shift s1 = db.getShift(1);
-        Shift s2 = db.getShift(2);
-        Shift s3 = db.getShift(3);
-        
-        System.out.println(s1.toString());
-        System.out.println(s2.toString());
-        System.out.println(s3.toString()); */
         
         db.closeConnection();
     
